acfef35043fc342eadd3181e80d26290c8c429a0
Govinda Biswas <75020328+prem-pb@users.noreply.github.com>
2023-11-02 19:10:38 +0530

    Url remove

--- config/config.js
+++ config/config.js
@@ -1,7 +1,7 @@
 import mongoose from "mongoose";
 
 export const CONNECTDB = (url) => {
-    mongoose.connect("mongodb+srv://prembiswas0703:prembiswas0703@cluster0.7nidp6s.mongodb.net/?retryWrites=true&w=majority").then(() => {
+    mongoose.connect("database url").then(() => {
         console.log("database is connected");
     }).catch((err) => {
         console.log(err);
